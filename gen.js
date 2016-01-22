271e5b8f458952e5b117b5f012f51ed4f882f362
Eric White <ewhite@ciena.com>
2016-01-22 16:35:13 -0500

    Actual generate file

---
+++ gen.js
@@ -0,0 +1,85 @@
+var fs = require('fs');
+var child_process = require('child_process');
+var chalk = require('chalk');
+var path  = require('path');
+var mark_dir = "markdown"
+var files = [];
+
+var routing_string = "module.exports = [ \n";
+
+function dive(dir){
+  // Read the directory
+
+  fs.readdir(dir, function (err, list) {
+    // Return the error if something went wrong
+    if (err)
+      return action(err);
+
+
+    // For every file in the list
+    list.forEach(function (file) {
+      // Full path of that file
+      var path = dir + "/" + file;
+      // Get the file's stats
+      fs.stat(path, function (err, stat) {
+        // console.log(stat);
+        // If the file is a directory
+        if (stat && stat.isDirectory()){
+          // Dive into the directory
+          console.log(chalk.red.bold("Directory: " + path.replace(mark_dir + "/","app/pods/")));
+          // uncomment when ready
+          // mkdirpSync(path.replace(mark_dir + "/","app/pods/"));
+          // {id: 'overview', alias: 'Overview', type: 'category', route: 'overview',
+          var filename = file.replace(".md","");
+          console.log(list);
+          routing_string += "{id: '"+ filename +"', alias: '" + toTitleCase(filename) + "', type: 'category', route: '" + path.replace(mark_dir + "/","").replace("/",".") + "', items: [ \n";
+          //write items: [
+          dive(path);
+
+          routing_string += "]},\n";
+        }else{
+          // {id: 'environment', alias: 'Environment', type: 'route', route: 'development.environment'},
+          var filename = file.replace(".md","");
+          routing_string += "{id: '"+ filename +"', alias: '" + toTitleCase(filename) + "', type: 'category', route: '" + path.replace(mark_dir + "/","").replace("/",".") + "'},"
+          // Call the action
+          var pagePath = dir.replace(mark_dir+ "/", "app/pods/") + "/" + file.replace(".md","");
+          console.log(chalk.green.bold("Create Folder page: " + pagePath));
+
+          console.log(chalk.blue.bold("Create Index Folder: " + pagePath + "/index"));
+
+          console.log(chalk.blue.bold("Create route.js: " + pagePath + "/index" + "/route.js"));
+
+          console.log(chalk.blue.bold("Create controller: " + pagePath + "/controller.js" ));
+
+          console.log(chalk.blue.bold("Create template: " + pagePath + "/template.hbs"));
+        }
+      });
+    });
+  });
+};
+
+var mkdirSync = function (path) {
+  try {
+    fs.mkdirSync(path);
+  } catch(e) {
+    if ( e.code != 'EEXIST' ) throw e;
+  }
+}
+
+var mkdirpSync = function (dirpath) {
+  var parts = dirpath.split(path.sep);
+  for( var i = 1; i <= parts.length; i++ ) {
+    mkdirSync( path.join.apply(null, parts.slice(0, i)) );
+  }
+}
+
+function toTitleCase(str)
+{
+    console.log(str);
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+console.log("Hello");
+dive(mark_dir);
+routing_string += "];";
+debugger;
+console.log(routing_string);
