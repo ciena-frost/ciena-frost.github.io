1d1bbc9fa15f6f0044e6883e9d2779364f3cd22d
Lafleur, Justin <jlafleur@ciena.com>
2016-01-25 15:08:10 -0500

    Working towards using prefixes on folders and files to select the order of the tabs.

--- generate.js
+++ generate.js
@@ -27,25 +27,7 @@ var mkdirpSync = function(dirpath) {
 var routing_string = "module.exports = [ \n";
 dive(mark_dir);
 routing_string += "];";
-
-var overviewIndexStart = routing_string.indexOf("{id: 'overview', alias: 'Overview', type: 'category', route: 'overview', items: [");
-var overviewIndexEnd = routing_string.indexOf("]},//overview",overviewIndexStart) + 14;
-var overviewString = routing_string.substring(overviewIndexStart, overviewIndexEnd);
-
-var developmentIndexStart = routing_string.indexOf("{id: 'development', alias: 'Development', type: 'category', route: 'development', items: [");
-var developmentIndexEnd = routing_string.indexOf("]},//development",developmentIndexStart) + 17;
-var developmentString = routing_string.substring(developmentIndexStart, developmentIndexEnd);
-console.log(developmentString);
-
-routing_string = routing_string.replace(overviewString, "");
-routing_string = routing_string.replace(developmentString, "");
-
-
-var routingStartIndex = routing_string.indexOf("[") + 3;
-
-var routing_string = [routing_string.slice(0, routingStartIndex), overviewString.concat(developmentString), routing_string.slice(routingStartIndex)].join('');
-
-console.log(routing_string);
+//console.log(routing_string);
 fs.writeFileSync("config/routing.js", routing_string);
 
 var list = [];
@@ -54,6 +36,9 @@ function dive(dir) {
   // Read the directory
   list = fs.readdirSync(dir);
   list.forEach(function(file) {
+    if(file.charAt(2) != "-")
+      return;
+
     // Full path of that file
     var path = dir + "/" + file;
     // Get the file's stats
@@ -79,6 +64,10 @@ function dive(dir) {
       routing_string += "\t{id: '" + filename + "', alias: '" + toTitleCase(filename) + "', type: 'route', route: '" + path.replace(mark_dir + "/", "").replaceAll("/", ".").replace(".md","") + "'},\n";
       // Call the action
       var pagePath = dir.replace(mark_dir, "app/pods") + "/" + file.replace(".md", "");
+
+      //remove the order prefixes
+      pagePath = pagePath.replaceAll("[0-9][0-9][-]", "");
+
       console.log(chalk.green.bold("Create Folder page: " + pagePath));
 
       console.log(chalk.blue.bold("Create Index Folder: " + pagePath + "/index"));
