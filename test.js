12ffda5905e5a9ac2e6477084d3db948b44c65c3
Eric White <ewhite@ciena.com>
2016-01-26 15:56:14 -0500

    Test.js

--- test.js
+++ test.js
@@ -2,6 +2,7 @@ var npm = require('npm');
 var http = require('http');
 var fs = require('fs');
 var request = require('sync-request');
+var chalk = require('chalk');
 
 var options = {
   'headers': {
@@ -9,104 +10,81 @@ var options = {
   }
 };
 
+
+
 var res = request('GET', 'https://api.github.com/orgs/ciena-frost/repos', options);
 var body = JSON.parse(res.getBody());
 
 body.forEach(function(repo) {
   console.log(repo.name);
-  if (repo.name.startsWith("ember-")){
+  if (repo.name.startsWith("ember-")) {
     //ember install this package
-    console.log("Doing Ember Install of : " + repo.name);
-    npm.load({
-      loaded: false
-    }, function (err) {
-      // catch errors
-      npm.commands.install([repo.name], function (er, data) {
-        console.log(er);
-      });
-      npm.on("log", function (message) {
-        // log the progress of the installation
-        console.log(message);
-      });
-    });
 
+    npmInstall(repo);
 
 
     //get Package JSON un comment when needed
+    package_url = repo.contents_url.replace("{+path}","package.json");
+    packageJSON = getPackageJSON(package_url);
+    demoParentDirectory = packageJSON.frost_guide_directory;
+    console.log(packageJSON);
+
+    readme_url = repo.contents_url.replace("{+path}", "README.md");
+    readme_content = getREADME(readme_url);
+
+    if (demoParentDirectory !== undefined && directoryExistsSync("/app/pods/" + demoParentDirectory)){
+      
+    }else{
+      console.log(chalk.red.bold("Directory: " + "/app/pods/" + demoParentDirectory + " does not exist. Skipping repo demo generation"));
+    }
 
 
-    // package_url = repo.contents_url.replace("{+path}","package.json?ref=dev");
-    // packageJSON = getPackageJSON(package_url);
-    // console.log(packageJSON);
-
+    // console.log(readme_content);
   }
 });
 
-function getPackageJSON(url){
+function getPackageJSON(url) {
   //get api file request
   var res = request('GET', url, options);
   var body = JSON.parse(res.getBody());
-  res = request('GET',body.download_url,options);
+  res = request('GET', body.download_url, options);
   return JSON.parse(res.getBody());
   //get download url
 
 }
-// requestify.get('https://api.github.com/repos/ciena-frost/ember-frost-button/readme').then(function(response) {
-//     // Get the response body
-//     var body = response.getBody();
-//     body.forEach(function(repo){
-//       console.log(repo.name);
-//       console.log("Download url: " + repo.download_url);
-//       var file = fs.createWriteStream(repo.name);
-//       var request = http.get(repo.download_url, function(response) {
-//         response.pipe(file);
-//       });
-//     });
-// // });
-// function getOrganization(url) {
-//   var content_url = "";
-//   var flag = false;
-//   var request = httpSync.request(url);
-//   var timedout = false;
-//   req.setTimeout(10, function() {
-//     console.log("Request Timedout!");
-//     timedout = true;
-//   });
-//   var response = request.end();
-//
-//   if (!timedout) {
-//     console.log(response);
-//     console.log(response.body.toString());
-//   } else {
-//     var body = respone.body;
-//     body.forEach(function(repo) {
-//       console.log(repo.name);
-//       console.log("Last Push: " + repo.pushed_at);
-//       if (repo.name != "ciena-frost.github.io") {
-//         content_url = repo.contents_url.replace("{+path}", "");
-//         flag = true;
-//       }
-//     });
-//   }
 
+function getREADME(url) {
+  //get api file request
+  var res = request('GET', url, options);
+  var body = JSON.parse(res.getBody());
+  var buf = new Buffer(body.content, 'base64')
+  return buf.toString("ascii");
 
-// requestify.get(url).then(function(response) {
-//     // Get the response body
-//     var body = response.getBody();
-//     body.forEach(function(repo){
-//       console.log(repo.name);
-//       console.log("Last Push: " + repo.pushed_at);
-//       if (repo.name != "ciena-frost.github.io"){
-//         content_url = repo.contents_url.replace("{+path}","");
-//         flag = true;
-//       }
-//     });
-// });
-//
-// while(flag==false)
-// {}
-// getRepo(content_url);
+}
 
+function npmInstall(repo) {
+  console.log("Doing Ember Install of : " + repo.name);
+  npm.load({
+    loaded: false
+  }, function(err) {
+    // catch errors
+    npm.commands.install([repo.name], function(er, data) {
+      console.log(er);
+    });
+    npm.on("log", function(message) {
+      // log the progress of the installation
+      console.log(message);
+    });
+  });
+}
+
+function directoryExistsSync(filePath) {
+    try {
+        return fs.statSync(filePath).isDirectory();
+    } catch (err) {
+        return false;
+    }
+}
 
 function get(url) {
   requestify.get(url).then(function(response) {
