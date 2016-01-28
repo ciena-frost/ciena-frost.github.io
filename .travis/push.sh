1a0aa001810d923156033e6ad747a7b994ec2b55
Eric White <ericwhite613@gmail.com>
2016-01-28 16:07:59 -0500

    Updated travis yml added push script

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
 
 setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
+  git config --global user.email "ericwhite613@gmail.com"
+  git config --global user.name "Eric White"
 }
 
 commit_website_files() {
-  git checkout -b gh-pages
-  git add . *.html
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  git add .
+  git commit --message "[ci skip]"
+  git push
 }
 
-upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages gh-pages 
-}
+#upload_files() {
+#  
+#}
 
 setup_git
 commit_website_files
-upload_files
+#upload_files
