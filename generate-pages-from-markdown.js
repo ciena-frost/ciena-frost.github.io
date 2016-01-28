2348553543c03c3469db5eaf9538d73c82a2f6cb
Eric White <ericwhite613@gmail.com>
2016-01-28 09:23:20 -0500

    Rename generation file

---
+++ generate-pages-from-markdown.js
@@ -0,0 +1,137 @@
+String.prototype.replaceAll = function (search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+};
+
+///////////////////////////MAIN////////////////////////////////
+var fs = require('fs');
+var child_process = require('child_process');
+var chalk = require('chalk');
+var path = require('path');
+var mark_dir = "markdown"
+var routing_string = "module.exports = [ \n";
+
+dive(mark_dir);
+
+routing_string += "];";
+
+//debug console.log(routing_string);
+fs.writeFileSync("config/routing.js", routing_string);
+//////////////////////////////////////////////////////////////
+
+function dive(dir) {
+    var list = [];
+    var stat = "";
+
+    // Read the directory
+    list = fs.readdirSync(dir);
+    list.forEach(function (file) {
+        if (file.charAt(2) != "-") {
+            console.log("\nFile: " + file + " does not have a proper page order prefix in its name. " +
+                "The page will not be generated.\n")
+            return;
+        }
+
+        // Full path of that file
+        var path = dir + "/" + file;
+
+        // Get the file's stats
+        stat = fs.statSync(path);
+
+        // If the file is a directory
+        if (stat && stat.isDirectory()) {
+            // Dive into the directory
+            //debug console.log(chalk.red.bold("Directory: " + path.replace(mark_dir + "/", "app/pods/")));
+
+            var filename = file.replace(".md", "");
+
+            routing_string += "{id: '" + filename.replaceAll("[0-9][0-9][-]", "") +
+                "', alias: '" + toTitleCase(filename.replaceAll("[0-9][0-9][-]", "").replaceAll("-", " ")) +
+                "', type: 'category', route: '" +
+                path.replace(mark_dir + "/", "").replaceAll("/", ".").replaceAll("[0-9][0-9][-]", "") +
+                "', items: [ \n";
+
+            dive(path);
+
+            routing_string += "\n]}, // " + filename + "\n"
+        } else {
+            var filename = file.replace(".md", "");
+
+            routing_string += "\t{id: '" + filename.replaceAll("[0-9][0-9][-]", "") + "', alias: '" +
+                filename.replaceAll("[0-9][0-9][-]", "").replaceAll("-", " ") +
+                "', type: 'route', route: '" +
+                path.replace(mark_dir + "/", "").replaceAll("/", ".").replace(".md", "").replaceAll("[0-9][0-9][-]", "") +
+                "'},\n";
+
+            var pagePath = dir.replace(mark_dir, "app/pods") + "/" + file.replace(".md", "");
+
+            //remove the order prefixes
+            pagePath = pagePath.replaceAll("[0-9][0-9][-]", "");
+
+            //debug console.log(chalk.green.bold("Create Folder page: " + pagePath));
+            //debug console.log(chalk.blue.bold("Create Index Folder: " + pagePath + "/index"));
+
+            if (!directoryExistsSync(pagePath + "/index")) {
+                mkdirpSync((pagePath + "/index").toLowerCase());
+            }
+
+            //debug console.log(chalk.blue.bold("Create route.js: " + pagePath + "/index" + "/route.js"));
+            var route_js_string = "import Ember from'ember';\nexport default Ember.Route.extend({\n\tbreadCrumb:{\n\t\ttitle:'" +
+                filename.replaceAll("[0-9][0-9][-]", "") + "'\n\t}\n});"
+
+            fs.writeFileSync(pagePath + "/index" + "/route.js", route_js_string);
+
+            //debug console.log(chalk.blue.bold("Create controller: " + pagePath + "/controller.js"));
+            //debug console.log(chalk.blue.bold("Create template: " + pagePath + "/template.hbs"));
+
+            fs.writeFileSync(pagePath + "/template.hbs", "{{markdown-to-html class=\"guide-markdown\" " +
+                "markdown=(fr-markdown-file-strip-number-prefix '" +
+                path.replace(".md", "").replace(mark_dir + "/", "").replaceAll("[0-9][0-9][-]", "") +
+                "')}}");
+        }
+
+    });
+}
+
+
+String.prototype.replaceAll = function (search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+};
+
+function fileExistsSync(filePath) {
+    try {
+        return fs.statSync(filePath).isFile();
+    } catch (err) {
+        return false;
+    }
+}
+
+function directoryExistsSync(filePath) {
+    try {
+        return fs.statSync(filePath).isDirectory();
+    } catch (err) {
+        return false;
+    }
+}
+
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
+
+function mkdirSync(path) {
+    try {
+        fs.mkdirSync(path);
+    } catch (e) {
+        if (e.code != 'EEXIST') throw e;
+    }
+}
+
+function mkdirpSync(dirpath) {
+    var parts = dirpath.split(path.sep);
+    for (var i = 1; i <= parts.length; i++) {
+        mkdirSync(path.join.apply(null, parts.slice(0, i)));
+    }
+}
