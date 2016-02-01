095f4a916f141b41799487c17e76e757e97591b6
Eric White <ericwhite613@gmail.com>
2016-02-01 14:04:12 -0500

    routes

---
+++ app/pods/ui-components/menu-controls/context-menu/route.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+export default Ember.Route.extend({
+	breadCrumb:{
+		title:'Context Menu'
+	},
+	actions: { 
+ 		 goTo:function(id){$('html, body').animate({scrollTop:$(id).offset().top},500)}}
+});
