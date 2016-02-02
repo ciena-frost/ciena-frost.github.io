f6c0de59298b9ab9be00a29ef525baa9e6df66ae
Eric White <ericwhite613@gmail.com>
2016-02-02 11:14:21 -0500

    add frost-tabs

---
+++ frost-components/frost-tabs/tests/dummy/app/pods/demo/controller.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+	queryParams: ['selectedTab'],
+
+	selectedTab: 'controller',
+
+	actions: {
+		tabSelected(tab) {
+			this.set('selectedTab', tab);
+		},
+    outerTabSelected(tab) {
+			this.set('selectedOuterTab', tab);
+		}
+	}
+});
