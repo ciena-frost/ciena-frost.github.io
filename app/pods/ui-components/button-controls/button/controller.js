8481c448b059f61aa4d77f47bf0cbbda642ba371
Eric White <ericwhite613@gmail.com>
2016-02-03 14:33:33 +0000

    [ci skip] Generating files/folders

--- app/pods/ui-components/button-controls/button/controller.js
+++ app/pods/ui-components/button-controls/button/controller.js
@@ -1,15 +1,15 @@
 import ApiController from 'frost-guide/utils/ApiController';
-import Ember from 'ember';
+import Ember from 'ember'
 
 export default ApiController.extend({
   actions: {
-    action() {
+    action () {
       this.notifications.addNotification({
         message: 'Action sent',
         type: 'success',
         autoClear: true,
         clearDuration: 2000
-      });
+      })
     }
   }
-});
+})
