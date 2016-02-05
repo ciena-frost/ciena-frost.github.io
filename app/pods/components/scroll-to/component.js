16419cb72c23a9ad6439b3f01ddddab5109dcec6
Justin Lafleur <lafleur.justin@gmail.com>
2016-02-05 14:10:26 -0500

    Nice left blue border on scroll spy for active tab

--- app/pods/components/scroll-to/component.js
+++ app/pods/components/scroll-to/component.js
@@ -8,8 +8,10 @@ export default Ember.Component.extend({
     $('#' + this.elementId).parent().children().each(function () {
       console.log(this.id)
       $(this).css('font-weight', 'normal')
+      $(this).css('border-left', '3px solid white')
     })
     $('#' + this.elementId).css('font-weight', 'bold')
+    $('#' + this.elementId).css('border-left', '3px solid #009EEF')
     $('html, body').animate({scrollTop: $(this.to).offset().top - (0.3 * $(window).height())}, 500)
     this.previousActiveId = this.elementId
   }
