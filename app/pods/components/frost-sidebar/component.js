f4cf2817b14907e3629124c89ee60d2545917334
White, Eric <ewhite@ciena.com>
2016-02-16 11:36:08 -0500

    Work on sidebar toggle change height

--- app/pods/components/frost-sidebar/component.js
+++ app/pods/components/frost-sidebar/component.js
@@ -8,6 +8,13 @@ export default Ember.Component.extend({
   actions: {
     toggleCollapsed () {
       this.toggleProperty('collapsed')
+      if (this.collapsed){
+        $('.frost-sidebar').height($('.frost-info-bar').height());
+      }else {
+        $('.frost-sidebar').height('100%')
+        $('.frost-navigation-sidebar-menu').css('background-color','#DDDDDD')
+        console.log("Changed background");
+      }
     }
   }
 })
