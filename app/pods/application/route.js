73bc9360bc152bde90b178628334dd362530f368
Lafleur, Justin <jlafleur@ciena.com>
2016-02-16 10:42:44 -0500

    footer and scrollspy minor css edits

--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -15,10 +15,13 @@ export default Ember.Route.extend({
         var docHeight = $(window).height();
         var footerHeight = $('.footer').height();
         var footerTop = $('.footer').position().top + footerHeight;
+        
 
         if (footerTop < docHeight) {
           $('.footer').css('margin-top', (docHeight - footerTop) + 'px');
         }
+        
+        $('.footer').css('visibility', 'visible');
       })
     }
   }
