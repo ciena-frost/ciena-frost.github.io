7170ba68c1abf9c9bd7e7a93165cb94d45b0ee82
White, Eric <ewhite@ciena.com>
2016-02-16 15:40:38 -0500

    Temp solution for overlaid text in mobile (line 17)

--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -3,17 +3,18 @@ import Ember from 'ember'
 export default Ember.Route.extend({
   actions: {
     didTransition: function () {
-      Ember.run.next(() => {
+      Ember.run.schedule('afterRender', this, function () {
         /*eslint-disable */
         Prism.highlightAll()
-        /*eslint-enable */
+          /*eslint-enable */
         if (!this.get('media.isMobile')) {
           $('.guide-sidebar').height($("[class='content']").height())
-        }else{
+        } else {
           $('.content').width($(window).width())
           $('.guide-markdown').width($(window).width())
           $('.frost-tabs').width($(window).width())
           $('#demo').width($(window).width())
+          $($('.title')[0]).css('margin-left', $($('.menu .collapsed')[0]).width() + 5 + 'px')
         }
 
         /*push footer to bottom of viewport if page has no y-overflow*/
