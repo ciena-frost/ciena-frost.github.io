485956032ae9edf1f535deca1bae61d1c78edca8
Lafleur, Justin <jlafleur@ciena.com>
2016-02-16 13:32:06 -0500

    make background sidebar blue

--- app/pods/components/frost-sidebar/component.js
+++ app/pods/components/frost-sidebar/component.js
@@ -5,16 +5,15 @@ export default Ember.Component.extend({
 
   collapsed: true,
   didInsertElement: function (){
-
+        $('.guide-sidebar-mobile').height($('.frost-info-bar').height())
   },
   actions: {
     toggleCollapsed () {
       this.toggleProperty('collapsed')
       if (this.collapsed){
-        $('.guide-sidebar-mobile').height($('.frost-info-bar').height());
+        $('.guide-sidebar-mobile').height($('.frost-info-bar').height())
       }else {
         $('.guide-sidebar-mobile').height('100%')
-        $('.frost-navigation-sidebar-menu').css('background-color','#DDDDDD')
       }
     }
   }
