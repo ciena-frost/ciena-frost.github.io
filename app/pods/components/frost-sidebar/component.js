a3b818ba02b8094d50fa379607daf6e87045fd28
Lafleur, Justin <jlafleur@ciena.com>
2016-02-16 14:32:59 -0500

    resize mobile menu collapsed according to info bar on window resize

--- app/pods/components/frost-sidebar/component.js
+++ app/pods/components/frost-sidebar/component.js
@@ -2,10 +2,13 @@ import Ember from 'ember'
 
 export default Ember.Component.extend({
   classNames: ['frost-sidebar'],
-
   collapsed: true,
+  windowRezise: function () {
+    $(window).on('resize', function () {
+      $('.guide-sidebar-mobile').height($('.frost-info-bar').height())
+    })
+  }.on('init'),
   didInsertElement: function (){
-    $('.guide-sidebar-mobile').height($('.frost-info-bar').height())
     $('.guide-sidebar-mobile').height($('.frost-info-bar').height())
   },
   actions: {
