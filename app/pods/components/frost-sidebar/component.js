81e6d319e9cae3b34f4609d0f550784a89c3ea99
White, Eric <ewhite@ciena.com>
2016-02-17 09:47:43 -0500

    Also resize content when inserted element ( Helps when refreshing on a page)

--- app/pods/components/frost-sidebar/component.js
+++ app/pods/components/frost-sidebar/component.js
@@ -3,11 +3,17 @@ import Ember from 'ember'
 export default Ember.Component.extend({
   classNames: ['frost-sidebar'],
   collapsed: true,
+  didInsertElement: function (){
+    Ember.run.schedule('afterRender', this, function () {
+      if (this.collapsed){
+        $('.md').width($(window).width() - $('.menu .collapsed').width())
+      }
+    })
+  },
   actions: {
     toggleCollapsed () {
       this.toggleProperty('collapsed')
       Ember.run.schedule('afterRender', this, function () {
-      console.log("Rendered");
       if (this.collapsed){
         $('.md').width($(window).width() - $('.menu .collapsed').width())
       }
