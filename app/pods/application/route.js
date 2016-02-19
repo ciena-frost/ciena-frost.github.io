177b86cbc975e9f7496295b94552c4f70f7d1204
White, Eric <ewhite@ciena.com>
2016-02-19 11:34:35 -0500

    Change query footer stuff

--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -19,7 +19,7 @@ export default Ember.Route.extend({
 
 
           if (footerTop < docHeight) {
-            $('.footer').css('margin-top', (docHeight - footerHeight) + 'px')
+            $('.footer').css('margin-top', (docHeight - ($('.frost-info-bar').height() + footerHeight)) + 'px')
           }
 
           $('.footer').css('visibility', 'visible');
