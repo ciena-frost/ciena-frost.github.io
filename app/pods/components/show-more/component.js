e573f650507e826049b1dd6c729973318085f9af
Lafleur, Justin <jlafleur@ciena.com>
2016-02-24 17:06:51 -0500

    Contributor layout ✓

--- app/pods/components/show-more/component.js
+++ app/pods/components/show-more/component.js
@@ -2,23 +2,22 @@ import Ember from 'ember'
 
 export default Ember.Component.extend({
   classNames: ['show-more'],
+  tagName: 'div',
   toggle: true,
-  moreOrLess: 'more',
+  moreOrLess: 'show more',
     actions: {
       showMore () {
         if(this.toggle){
           $('#' + this.elementId + ' .more').css('display', 'inline');
-          $('#' + this.elementId + ' .more').css('background-color', 'white');
-          $('#' + this.elementId + ' .more').css('z-index', '9999');
+          $('#' + this.elementId).css('z-index', '1');
           this.set('toggle', false);
-          this.set('moreOrLess', 'less');
-          console.log('changeToless')
+          this.set('moreOrLess', 'show less');
         }
         else{
           $('#' + this.elementId + ' .more').css('display', 'none');
           this.set('toggle', true);
-          this.set('moreOrLess', 'more');
-          console.log('changeToMore')
+          this.set('moreOrLess', 'show more');
+          $('#' + this.elementId).css('z-index', '');
         }
       }
     }
