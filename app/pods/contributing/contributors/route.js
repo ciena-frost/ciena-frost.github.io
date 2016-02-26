76b92ed557e413b6daa29a540e7cb7998c5a3e2f
Lafleur, Justin <jlafleur@ciena.com>
2016-02-26 15:25:18 -0500

    model and template each

--- app/pods/contributing/contributors/route.js
+++ app/pods/contributing/contributors/route.js
@@ -1,6 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    return [{
+         handle: 'laflower',
+         name: 'Justin Lafleur',
+         avatar: 'https://avatars.githubusercontent.com/u/11548575?v=3',
+         repos: ['frost-button', 'frost-css', 'frost-button', 'frost-css', 'frost-button', 'frost-css', 'frost-button', 'frost-css']
+      }, {
+         handle: 'laflower',
+         name: 'Justin Lafleur',
+         avatar: 'https://avatars.githubusercontent.com/u/11548575?v=3',
+         repos: ['frost-button', 'frost-css', 'frost-button', 'frost-css', 'frost-button', 'frost-css', 'frost-button', 'frost-css']
+      }, {
+         handle: 'laflower',
+         name: 'Justin Lafleur',
+         avatar: 'https://avatars.githubusercontent.com/u/11548575?v=3',
+         repos: ['frost-button', 'frost-css', 'frost-button', 'frost-css', 'frost-button', 'frost-css', 'frost-button', 'frost-css']
+      }, {
+         handle: 'laflower',
+         name: 'Justin Lafleur',
+         avatar: 'https://avatars.githubusercontent.com/u/11548575?v=3',
+         repos: ['frost-button', 'frost-css', 'frost-button', 'frost-css', 'frost-button', 'frost-css', 'frost-button', 'frost-css']
+      }]
+      },
   breadCrumb: {
     title: 'Contributors'
   }
