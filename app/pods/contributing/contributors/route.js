77e58d802581a334b37ebc9354729df6481ecb90
White, Eric <ewhite@ciena.com>
2016-02-29 15:36:49 -0500

    Bad Megre fix

--- app/pods/contributing/contributors/route.js
+++ app/pods/contributing/contributors/route.js
@@ -3,27 +3,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function () {
-    return $.getJSON("data/contributors.json")
-      avatar: 'https://avatars.githubusercontent.com/u/11548575?v=3',
-      repos: ['frost-button', 'frost-css', 'frost-button', 'frost-css', 'frost-button', 'frost-css', 'frost-button', 'frost-css']
-      }, {
-      handle: 'ewhte613',
-      name: 'Eric Whitesss',
-      avatar: 'https://avatars.githubusercontent.com/u/11548575?v=3',
-      repos: ['frost-button', 'frost-css', 'frost-button', 'frost-css', 'frost-button', 'frost-css', 'frost-button', 'frost-css']
-      }, {
-      handle: 'ewhite63',
-      name: 'Eric White',
-      avatar: 'https://avatars.githubusercontent.com/u/11548575?v=3',
-      repos: ['frost-button', 'frost-css', 'frost-button', 'frost-css', 'frost-button', 'frost-css', 'frost-button', 'frost-css']
-      }, {
-      handle: 'ewhite61',
-      name: 'Eric White',
-      avatar: 'https://avatars.githubusercontent.com/u/11548575?v=3',
-      repos: ['frost-button', 'frost-css', 'frost-button', 'frost-css', 'frost-button', 'frost-css', 'frost-button', 'frost-css']
-      }, {
-      handle: 'ewhe613',
-      name: 'Eric White',
+    return $.getJSON("data/contributors.json");
   },
   breadCrumb: {
     title: 'Contributors'
