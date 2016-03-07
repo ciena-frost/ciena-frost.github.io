6adc23fe7d4b85db77475a82a27e4419a2ac9d73
White, Eric <ewhite@ciena.com>
2016-03-07 15:21:34 -0500

    Fix variable for ember-frost-list

--- config/environment.js
+++ config/environment.js
@@ -9,7 +9,7 @@ module.exports = function (environment) {
     environment: environment,
     baseURL: '/frost-guide',
     locationType: 'hash',
-    isFrostGuide: true,
+    isFrostGuideDirectory: true,
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
