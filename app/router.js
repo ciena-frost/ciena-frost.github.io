64dd1d5ac27d26f2e6bbe8b18920e5a39594523b
White, Eric <ewhite@ciena.com>
2016-03-10 10:36:27 -0500

    Use frost-guide custom routing addon

--- app/router.js
+++ app/router.js
@@ -1,33 +1,11 @@
 import Ember from 'ember'
 import config from './config/environment'
+import addRoute from 'frost-guide-custom-routing/utils/addRoute'
 
 var Router = Ember.Router.extend({
   location: config.locationType
 })
 
-var addRoute = function (routeConfig) {
-  if (Ember.isEmpty(routeConfig.items)) {
-    if (routeConfig.path !== undefined && routeConfig.modalName !== undefined) {
-      this.route(routeConfig.id, routeConfig.path, function () {
-        this.modal(routeConfig.modalName, routeConfig.modal)
-      })
-    } else if (routeConfig.path !== undefined) {
-      this.route(routeConfig.id, routeConfig.path)
-    } else if (routeConfig.modalName !== undefined) {
-      this.route(routeConfig.id, function () {
-        this.modal(routeConfig.modalName, routeConfig.modal)
-      })
-    } else {
-      this.route(routeConfig.id)
-    }
-  } else {
-    this.route(routeConfig.id, function () {
-      routeConfig.items.forEach((item) => {
-        addRoute.call(this, item)
-      })
-    })
-  }
-}
 
 Router.map(function () {
   let routerConfig = config.APP.routingConfig
