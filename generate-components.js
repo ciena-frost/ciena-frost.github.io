f37e3dc154f9704f48e8c12ea3fca481af2c5e62
White, Eric <ewhite@ciena.com>
2016-03-25 15:07:09 -0400

    Add button to ignore list and update package.json

--- generate-components.js
+++ generate-components.js
@@ -26,7 +26,7 @@ Array.prototype.contains = function (obj) {
   return false;
 }
 
-var ignoreList = ['ember-frost-bunsen', 'ember-frost-checkbox', 'ember-frost-brackets-snippets']
+var ignoreList = ['ember-frost-bunsen', 'ember-frost-checkbox', 'ember-frost-brackets-snippets', 'ember-frost-button']
 
 var options = {
   'headers': {
