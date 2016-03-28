1535c46e9cb053b7e76244b65b5ab32068e3c4a9
White, Eric <ewhite@ciena.com>
2016-03-28 10:05:31 -0400

    Turn off progress bar for nom

--- .travis/push.sh
+++ .travis/push.sh
@@ -6,6 +6,7 @@ setup_git() {
   git config --global user.email "ericwhite613@gmail.com"
   git config --global user.name "Eric White"
   git config --global push.default simple
+  npm set progress=false
   git config credential.helper "store --file=.git/credentials"
   echo "https://${GH_TOKEN}:@github.com" > .git/credentials
   git fetch origin
