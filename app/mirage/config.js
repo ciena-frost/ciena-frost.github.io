904b4550893e9078976f64560071c5f906ee9708
Eric White <ericwhite613@gmail.com>
2016-03-29 16:42:57 +0000

    [ci skip] Generating files/folders

--- app/mirage/config.js
+++ app/mirage/config.js
@@ -1,6 +1,4 @@
-import emberFrostCore from './ember-frost-core-config'
 import config from '../config/environment'
 export default function () {
 	this.namespace = config.mirageNamespace
-	emberFrostCore.call(this)
 }
