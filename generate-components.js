bb47ebb947d385dd849c8651165277304893f169
White, Eric <ewhite@ciena.com>
2016-03-29 08:41:27 -0400

    More WIP on clone

--- generate-components.js
+++ generate-components.js
@@ -400,7 +400,7 @@ function getDemoMirage(url, scenariosToImportMap, configstoImportMap, repoName) 
         })
       } else {
         if (mirage.name === "config.js") {
-          var config_Content = getFile(mirage.url)
+          var config_Content = fs.readFileSync(mirage.url, 'utf8')
           var path = "app/mirage/" + repoName + "-config.js"
           fs.writeFileSync(path, config_Content)
           var val = repoName.replace(/-(.)/g, function (m, $1) {
