9683d95a055cb3bd9347e6684aea7fde9e9d84b9
White, Eric <ewhite@ciena.com>
2016-03-31 10:24:55 -0400

    Get rid of private images and image folders

--- external-publish.js
+++ external-publish.js
@@ -1,27 +1,150 @@
 var fs = require('fs');
 var path = require('path');
 
-var images = walkSync('public/assets/images')
-console.log(images)
+var args = process.argv.slice(2);
+var unMark = false;
+if (args.length >= 1) {
+  console.log('found param')
+  unMark = args[0] === 'unMark' ? true : false
+}
+if (unMark) {
+  var unMarkedFilesAndFolders = require('./deleted.json')
+  console.log('Putting back shit ')
+  unMarkedFilesAndFolders.forEach(function (item) {
+    if (Array.isArray(item.content)) {
+      putFolderBack(item)
+    } else {
+      putFileBack(item)
+    }
+  })
+} else {
+  var markedForDeletion = []
+  var images = walkSync('public/assets/images', '', markedForDeletion)
+
+  for (var i = 0; i < markedForDeletion.length; i++) {
+    var value = markedForDeletion[i]
+    if (value.content === 'folder') {
+      value.content = getDirContents(value.name)
+      rmDir(value.name, true)
+    } else {
+      //file
+      fs.unlinkSync(value.name);
+    }
+  }
+  fs.writeFileSync('deleted.json', JSON.stringify(markedForDeletion))
+}
+
+// function to encode file data to base64 encoded string
+function base64_encode(file) {
+    // read binary data
+    var bitmap = fs.readFileSync(file);
+    // convert binary data to base64 encoded string
+    return new Buffer(bitmap).toString('base64');
+}
+
+// function to create file from base64 encoded string
+function base64_decode(base64str, file) {
+    // create buffer object from base64 encoded string, it is important to tell the constructor that the string is base64 encoded
+    var bitmap = new Buffer(base64str, 'base64');
+    // write buffer to file
+    fs.writeFileSync(file, bitmap);
+    console.log('******** File created from base64 encoded string ********');
+}
+
+
+function putFileBack(file) {
+  base64_decode(file.content, file.name)
+}
+
+function putFolderBack(folder) {
+  if (!fs.existsSync(folder.name)) {
+    fs.mkdirSync(folder.name);
+  }
+  folder.content.forEach(function (item) {
+    if (Array.isArray(item.content)) {
+      putFolderBack(item)
+    } else {
+      putFileBack(item)
+    }
+  })
+}
 
 
 /*
  * Get directory folder and files
  */
-function walkSync(dir, parent) {
+function walkSync(dir, parent, markForDeletion) {
   parent = typeof parent !== 'undefined' ? parent : '';
   var fs = fs || require('fs');
   var files = fs.readdirSync(dir);
   var filelist = [];
   files.forEach(function (file) {
     if (fs.statSync(dir + '/' + file).isDirectory()) {
-      filelist.push({
-        folder: parent + file,
-        items: walkSync(dir + '/' + file, parent + file + '/')
-      });
+      if (file.indexOf('private') > -1) {
+        //mark for deletion
+        markForDeletion.push({
+          name: dir + '/' + file,
+          content: 'folder'
+        })
+      } else {
+        filelist.push({
+          folder: parent + file,
+          items: walkSync(dir + '/' + file, parent + file + '/', markForDeletion)
+        })
+      }
     } else {
+      if (file.indexOf('private') > -1) {
+        markForDeletion.push({
+          name: dir + '/' + file,
+          content: base64_encode(dir + '/' + file)
+        })
+      }
       filelist.push(parent + file);
     }
   });
   return filelist;
 };
+
+function getDirContents(currentDirPath, parent) {
+  parent = typeof parent !== 'undefined' ? parent : '';
+  var fs = fs || require('fs');
+  var files = fs.readdirSync(currentDirPath);
+  var filelist = [];
+  files.forEach(function (file) {
+    if (fs.statSync(currentDirPath + '/' + file).isDirectory()) {
+      filelist.push({
+        folder: parent + file,
+        items: getDirContents(currentDirPath + '/' + file, parent + file + '/')
+      })
+    } else {
+      filelist.push({
+        name: currentDirPath + '/' + file,
+        content: base64_encode(currentDirPath + '/' + file)
+      });
+    }
+  });
+  return filelist;
+}
+
+function rmDir(dirPath, removeSelf) {
+  if (removeSelf === undefined)
+    removeSelf = true;
+  try {
+    var files = fs.readdirSync(dirPath);
+  } catch (e) {
+    return;
+  }
+  if (files.length > 0)
+    for (var i = 0; i < files.length; i++) {
+      var filePath = dirPath + '/' + files[i];
+      if (filePath !== 'clones/.gitkeep') {
+        if (fs.statSync(filePath).isFile())
+          fs.unlinkSync(filePath);
+        else
+          rmDir(filePath);
+      }
+    }
+  if (removeSelf)
+    fs.rmdirSync(dirPath);
+};
+
