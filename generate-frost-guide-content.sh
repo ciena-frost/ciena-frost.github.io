6bb6deb1f158edacccd3cb32e54591d6b9c2d657
White, Eric <ewhite@ciena.com>
2016-03-31 08:48:52 -0400

    Fix travis ci folding

--- generate-frost-guide-content.sh
+++ generate-frost-guide-content.sh
@@ -1,9 +1,9 @@
-  echo -en 'travis_fold:start:#Generate-Roadmap\\r'
+  echo -en 'travis_fold:start:Generate_Roadmap\\r'
   node generate-roadmap.js
-  echo -en 'travis_fold:end:#Generate-Roadmap\\r'
-  echo -en 'travis_fold:start:#Generate-Pages-From-Markdown\\r'
+  echo -en 'travis_fold:end:Generate_Roadmap\\r'
+  echo -en 'travis_fold:start:Generate_Pages_From_Markdown\\r'
   node generate-pages-from-markdown.js
-  echo -en 'travis_fold:end:#Generate-Pages-From-Markdown\\r'
-  echo -en 'travis_fold:start:#Generate-Components\\r'
+  echo -en 'travis_fold:end:Generate_Pages_From_Markdown\\r'
+  echo -en 'travis_fold:start:Generate_Components\\r'
   node generate-components.js
-  echo -en 'travis_fold:end:#Generate-Components\\r'
+  echo -en 'travis_fold:end:Generate_Components\\r'
