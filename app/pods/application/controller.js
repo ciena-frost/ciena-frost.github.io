2acffe82e287c5330eca523405bbdc0bc661d4fa
Lafleur, Justin <jlafleur@ciena.com>
2016-03-31 16:41:07 -0400

    fix linting

--- app/pods/application/controller.js
+++ app/pods/application/controller.js
@@ -6,7 +6,6 @@ export default Ember.Controller.extend({
   applicationMenuItems: config.APP.routingConfig,
   actions: {
     toggleCollapsed () {
-      console.log("sending events to components")
       this.get('frostSidebar').send('toggleCollapsed') // send action to frost-sidebar component
     }
   }
--- app/pods/components/frost-footer/component.js
+++ app/pods/components/frost-footer/component.js
@@ -1,26 +1,26 @@
 import Ember from 'ember'
 
-MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-var posInProgress = false;
-var fillInProgress = false;
-  
-var observePositionFooter = new MutationObserver ( function() {
-  if(!posInProgress){
-    posInProgress = true;
+var MutationObserver = window.MutationObserver || window.WebKitMutationObserver
+var posInProgress = false
+var fillInProgress = false
+
+var observePositionFooter = new MutationObserver(function () {
+  if (!posInProgress) {
+    posInProgress = true
     positionFooter()
-    setTimeout(function(){
-      posInProgress = false;
-    }, 50);
+    setTimeout(function () {
+      posInProgress = false
+    }, 50)
   }
 })
 
-var observeFillFooterContent = new MutationObserver ( function() {
-  if(!fillInProgress){
-    fillInProgress = true;
+var observeFillFooterContent = new MutationObserver(function () {
+  if (!fillInProgress) {
+    fillInProgress = true
     fillFooterContent()
-    setTimeout(function(){
-      fillInProgress = false;
-    }, 50);
+    setTimeout(function () {
+      fillInProgress = false
+    }, 50)
   }
 })
 
@@ -31,52 +31,47 @@ function positionFooter () {
   var footerHeight = $('.footer').height()
   var footerTop = $('.footer').offset().top + footerHeight
   if (footerTop < docHeight) {
-    $('.footer').css('margin-top', (docHeight - ($('.frost-info-bar').height() 
-                                                 + $('.md').height() 
-                                                 + $('.contributors-outlet').height() 
-                                                 + $('.footer').height())))
+    $('.footer').css('margin-top', (docHeight - ($('.frost-info-bar').height() +
+                                                  $('.md').height() +
+                                                  $('.contributors-outlet').height() +
+                                                  $('.footer').height())))
   }
   $('.footer').css('visibility', 'visible')
-  if($('.guide-sidebar').offset().top + $('.guide-sidebar').height() > $('.footer').offset().top + $('.footer').height()){
+  if ($('.guide-sidebar').offset().top + $('.guide-sidebar').height() > $('.footer').offset().top + $('.footer').height()) {
     // the bottom of the sidebar is lower than the bottom of the footer
-    $('.guide-sidebar').css('top', $('.footer').offset().top + $('.footer').height() - $('.guide-sidebar').height())    
+    $('.guide-sidebar').css('top', $('.footer').offset().top + $('.footer').height() - $('.guide-sidebar').height())
   }
 }
 
 function fillFooterContent () {
- if($('#description').length && $('#api').length && $('#demo').length){
+  if ($('#description').length && $('#api').length && $('#demo').length) {
     // this is a component page
     $('.contributors .footerHeading').html('Contributors')
     $('.contributors .contributors-list').html($('.contributors-list-data').html())
     $('.connect .footerHeading').html('Connect')
     $('.connect .gh-link').html($('.gh-link-data').html())
-  }
-  else{
+  } else {
     $('.contributors .footerHeading').html('')
     $('.contributors .contributors-list').html('')
     $('.connect .footerHeading').html('')
     $('.connect .gh-link').html('')
   }
 }
-  
+
 export default Ember.Component.extend({
   didInsertElement () {
     positionFooter()
     fillFooterContent()
-    
     $(window).on('resize', function () {
       positionFooter()
     })
-    
     observePositionFooter.observe(document, {
       childList: true,
       subtree: true
-    });
-    
+    })
     observeFillFooterContent.observe(document, {
       childList: true,
       subtree: true
-    });
-  
+    })
   }
 })
