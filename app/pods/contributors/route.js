2a2f6a6f34a0cbec4d889c42d06b5d129fbfc68d
White, Eric <ewhite@ciena.com>
2016-04-01 08:26:03 -0400

    fix lint error

--- app/pods/contributors/route.js
+++ app/pods/contributors/route.js
@@ -3,7 +3,7 @@ import Ember from 'ember'
 export default Ember.Route.extend({
   actions: {
     didTransition: function () {
-      window.scrollTo(0,0)
+      window.scrollTo(0, 0)
     }
   }
 })
