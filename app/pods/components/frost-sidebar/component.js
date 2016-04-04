dcb259adcff4a49fc32f77bbe291b2fbdd81a5e3
Lafleur, Justin <jlafleur@ciena.com>
2016-04-04 11:14:56 -0400

    remove bad/old code to resize .md div

--- app/pods/components/frost-sidebar/component.js
+++ app/pods/components/frost-sidebar/component.js
@@ -3,11 +3,9 @@ import Ember from 'ember'
 export default Ember.Component.extend({
   classNames: ['frost-sidebar'],
   collapsed: true,
-  _resizeListener: null,
   _leftSwipeListener: null,
   _rightSwipeListener: null,
   didInsertElement: function () {
-    this._resizeListener = Ember.run.bind(this, this.adjustContent)
     this._rightSwipeListener = Ember.run.bind(this, this.openMenu)
     this._leftSwipeListener = Ember.run.bind(this, this.closeMenu)
     this.set('register-as', this) // allows to receive actions from application controller
@@ -28,12 +26,6 @@ export default Ember.Component.extend({
       Ember.$(window).off('resize', this._resizeListener)
     }
   },
-  adjustContent: function () {
-    if($('.md').width() > 400){
-      $('.md').width($(window).width() - $('.menu .collapsed').width())
-      $('.md').css('max-width', $(window).width() - $('.menu .collapsed').width())
-    }
-  },
   openMenu: function () {
     this.set('collapsed', false)
   },
@@ -43,11 +35,6 @@ export default Ember.Component.extend({
   actions: {
     toggleCollapsed () {
       this.toggleProperty('collapsed')
-      Ember.run.schedule('afterRender', this, function () {
-        if (this.collapsed) {
-          this.adjustContent()
-        }
-      })
     }
   }
 })
